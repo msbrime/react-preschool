35299f4c0c6997ba7319347b59dc1314790a1fd2
Salis Braimah <msbrime@gmail.com>
2017-03-06 11:22:46 +0000

    added router

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -4,7 +4,6 @@ export default class App extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(props);
 
         // let maxScore = this.getMaxScore(questions);
         // this.state = {
