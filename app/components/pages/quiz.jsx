4fec098875b628ed5f25079d2ccc46ed7b051782
Salis Braimah <msbrime@gmail.com>
2023-04-08 02:45:05 +0200

    phase 1 migration of quiz to hooks

--- app/components/pages/quiz.jsx
+++ app/components/pages/quiz.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
-import QuestionSet from 'containers/question-set.jsx'
+import React, {useState,useEffect} from 'react'
+import QuestionSet from 'containers/fn-question-set.jsx'
+import { load } from 'services/questions'
 
-const quiz = () => <QuestionSet />
+
+const quiz = () => {
+  const [quizState, updateQuizState] = useState({questions: {} , ids: [], maxScore: 0})
+
+  useEffect(() => {
+    load(({questions,id,maxScore}) => {
+      updateQuizState({questions,id,maxScore});
+    })
+  },[])
+
+  return Object.values(quizState.questions).length > 0 ?
+  <QuestionSet questions={quizState.questions} /> :
+  null
+}
 
 export default quiz
